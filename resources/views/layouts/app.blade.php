48c8a300c4cd26ab5c3e25c59ad8da6e7692421a
Henrik <henrik.dahlheim@gmail.com>
2016-09-20 06:38:10 +0200

    header user

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,34 +9,13 @@
     <body>
         @if (Auth::check())
         <header>
-
+            <a class="user" href="#">
+                <span class="user-avatar"></span>
+                <span class="user-name">{{ Auth::user()->name }}</span>
+                <span class="user-arrow"></span>
+            </a>
         </header>
         @endif
         @yield('content')
     </body>
 </html>
-
-{{-- @if (Auth::guest())
-    <li><a href="{{ url('/login') }}">Login</a></li>
-    <li><a href="{{ url('/register') }}">Register</a></li>
-@else
-    <li class="dropdown">
-        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-            {{ Auth::user()->name }} <span class="caret"></span>
-        </a>
-
-        <ul class="dropdown-menu" role="menu">
-            <li>
-                <a href="{{ url('/logout') }}"
-                    onclick="event.preventDefault();
-                             document.getElementById('logout-form').submit();">
-                    Logout
-                </a>
-
-                <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
-                    {{ csrf_field() }}
-                </form>
-            </li>
-        </ul>
-    </li>
-@endif --}}
